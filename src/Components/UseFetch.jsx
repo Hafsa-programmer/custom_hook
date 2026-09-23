a3a6c87d06159d39a78ae5330d8d57b1ac00cc5b
Hafsa-programmer <hafsasiddqui29@gmail.com>
2026-09-23 07:10:09 -0400

    Fetch data using custom hooks

--- src/Components/UseFetch.jsx
+++ src/Components/UseFetch.jsx
@@ -1,4 +1,11 @@
+import React, { useEffect, useState } from 'react'
 const UseFetch = (url) => {
+     const[data,setData]=useState();
+     useEffect(()=>{
+        fetch(url).then((res)=>res.json())
+        .then((data)=>setData(data))
+    },[url]);
+  return [data];
     
 }
 
